1fa1925d2d685ca190085f0c9a91d81851f8b382
TurboGLS <matteazzitomas@gmail.com>
2025-05-24 15:15:55 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'esame2426-fe';
+}
